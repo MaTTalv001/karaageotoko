ab6ed1f12ab3da2d82fdf818a18d64a67a85900e
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-16 00:09:28 +0900

    title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,16 @@ import { memo } from "react";
 export const Header = memo(() => {
   return (
     <header className="navbar bg-primary relative z-50">
+      <div className="navbar-start">
+      </div>
       <div className="navbar-center text-base-100 flex-1 justify-center">
         <Link to="/" className="btn btn-ghost normal-case text-xl md:text-2xl">
           からあげ様は揚げられたい
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <Link to="/ranking" className="btn btn-ghost normal-case text-base-100">
+          ランキング
         </Link>
       </div>
     </header>
