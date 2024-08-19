f5ef4e2e997e4f77e69a7adf7af34a976bc82265
MaTTalv001 <pomeranian1981@gmail.com>
2024-08-19 20:08:48 +0900

    title

--- src/constants/gameConfig.js
+++ src/constants/gameConfig.js
@@ -1,4 +1,4 @@
-export const INITIAL_PLAYER_POSITION = { x: 100, y: 300 };
+export const INITIAL_PLAYER_POSITION = { x: 100, y: 500 };
 export const JUMP_FORCE = 0.03;
 export const VELOCITY_THRESHOLD = 0.3; // この値以下の速度でのみジャンプ可能
 
